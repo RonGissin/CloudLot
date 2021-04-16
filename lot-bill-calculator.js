9f2339c040602a1306aaa71f77f0737dec69e96c
RonGissin <45989167+RonGissin@users.noreply.github.com>
2021-04-16 19:00:04 +0300

    fix calc

--- lot-bill-calculator.js
+++ lot-bill-calculator.js
@@ -8,7 +8,7 @@ class LotBillCalculator {
 	calculateBill(entryDate, exitDate) {
 		console.log(`entry date = ${entryDate}. exit date = ${exitDate}`);
 		const minDiff = getMinuteDiff(exitDate, entryDate);
-		const quarterHourUnits = minDiff / 15;
+		const quarterHourUnits = Math.round(minDiff / 15);
 		const bill = quarterHourUnits * 2.5;
 
 		console.log(`LotBillCalculator - bill is ${bill}`);
