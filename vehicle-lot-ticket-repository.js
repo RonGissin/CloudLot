1accb46ee04fc1be991571ee076336f16691008b
RonGissin <45989167+RonGissin@users.noreply.github.com>
2021-04-26 17:30:21 +0300

    Update vehicle-lot-ticket-repository.js

--- vehicle-lot-ticket-repository.js
+++ vehicle-lot-ticket-repository.js
@@ -27,6 +27,7 @@ class VehicleLotTicketRepository {
 			item = await this.dbclient.getItem(params);
 		} catch (e) {
 			console.log(e);
+			return null;
 		}
 
 		return item === null ? null : unmarshall(item.Item);
